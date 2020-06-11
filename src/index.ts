b5850590973262daa25f6615fe1390c5aefa412d
patomation <patomation@gmail.com>
2020-06-11 18:59:25 +0700

    feat: index entry uses multiple scenes

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,11 @@
 import 'phaser'
-import logo from './images/phaser3-logo.png'
-import libs from './images/libs.png'
-import plasmaBundle from './vendor/plasma-bundle.glsl'
-import starFields from './vendor/starfields.glsl'
-
-export default class Demo extends Phaser.Scene {
-  constructor () {
-    super('demo')
-  }
-
-  preload (): void {
-    this.load.image('logo', logo)
-    this.load.image('libs', libs)
-    this.load.glsl('bundle', plasmaBundle)
-    this.load.glsl('stars', starFields)
-  }
-
-  create (): void {
-    this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0)
-
-    this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0)
-
-    this.add.image(400, 300, 'libs')
-
-    const logo = this.add.image(400, 70, 'logo')
-
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
-      repeat: -1
-    })
-  }
-}
+import Loader from './scenes/Loader'
+import Title from './scenes/Title'
+import Game from './scenes/Game'
 
 const config = {
   type: Phaser.AUTO,
-  backgroundColor: '#125555',
+  backgroundColor: '#000000',
   // Stuff that makes the game responsive
   scale: {
     mode: Phaser.Scale.FIT,
@@ -47,7 +14,11 @@ const config = {
     width: 800,
     height: 600
   },
-  scene: Demo
+  scene: [
+    Loader,
+    Title,
+    Game
+  ]
 }
 
 const game = new Phaser.Game(config)
