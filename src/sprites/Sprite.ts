17181af9a7ef83807c580321c4c2589ecee74619
patomation <patomation@gmail.com>
2020-06-11 18:56:38 +0700

    feat: sprite class extension example added

---
+++ src/sprites/Sprite.ts
@@ -0,0 +1,34 @@
+export interface Options {
+  scene: Phaser.Scene
+  x: number
+  y: number,
+  key: string
+  owner?: string
+  rank?: number
+}
+export default class Sprite extends Phaser.GameObjects.Sprite {
+  constructor ({
+    scene,
+    x, y,
+    key
+  }: Options) {
+    super(scene, x, y, key)
+    // Adds object to scene automatically
+    scene.add.existing(this)
+    // Allow clicking ect
+    this.setInteractive()
+  }
+
+  public select (): void {
+    this.setTint(0x339900)
+  }
+
+  public deselect (): void {
+    this.clearTint()
+  }
+
+  public move (x: number, y: number): void {
+    this.x = x
+    this.y = y
+  }
+}
